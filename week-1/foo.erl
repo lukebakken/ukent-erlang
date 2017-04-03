27cedcaaf1adf8d1da880eb208601ac9b038dda1
Luke Bakken <luke@bakken.io>
2017-04-03 15:26:35 -0700

    add some more functions

--- week-1/foo.erl
+++ week-1/foo.erl
@@ -1,5 +1,6 @@
 -module(foo).
--export([bar/0, bar/1]).
+-export([bar/0, bar/1,
+         baz/0, bazz/0]).
 
 bar() ->
     timer:sleep(500),
@@ -11,3 +12,19 @@ bar(Pid) when is_pid(Pid) ->
     Pid ! "bar started~n",
     Pid ! "bar working~n",
     Pid ! "bar finished~n".
+
+baz() ->
+    receive
+        Msg ->
+            io:format("got: ~p~n", [Msg]),
+            baz()
+    end.
+
+bazz() ->
+    receive
+        stop ->
+            io:format("bazz stopped~n");
+        Msg ->
+            io:format("got: ~p~n", [Msg]),
+            bazz()
+    end.
